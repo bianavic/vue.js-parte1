089716fa016438fa32be2cc585dd20dab2b34840
bianavic <bianavic@gmail.com>
2019-09-10 19:55:01 -0300

    modulo VueRouter

--- app-pic/src/main.js
+++ app-pic/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
+// importando o router!
+import VueRouter from 'vue-router';
 
 Vue.use(VueResource);
+Vue.use(VueRouter); // registrando o router
 
 new Vue({
   el: '#app',
