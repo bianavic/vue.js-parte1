fe99434e50ff891a4604a9b0ea83ff87d7338f59
bianavic <bianavic@gmail.com>
2019-09-10 20:55:35 -0300

    componente transition

--- app-pic/src/main.js
+++ app-pic/src/main.js
@@ -4,10 +4,20 @@ import VueResource from 'vue-resource';
 // importando o router!
 import VueRouter from 'vue-router';
 
+// tem que vir entre chaves, porque não é default
+import { routes } from './routes';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes : routes,
+  mode: 'history' // adicionando a propriedade mode com o valor history.
+});
+
 Vue.use(VueResource);
-Vue.use(VueRouter); // registrando o router
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
