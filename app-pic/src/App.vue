62be628e136ae6ec20e3e0bb362a9283912e0a1f
bianavic <bianavic@gmail.com>
2019-09-10 17:00:59 -0300

    usando slot

--- app-pic/src/App.vue
+++ app-pic/src/App.vue
@@ -1,21 +1,15 @@
-<!-- alurapic/src/App.vue -->
-
 <template>
   <div class="corpo">
 
-    <h1 class="titulo">{{ titulo }}</h1>
+    <h1 class="centralizado">{{ titulo }}</h1>
 
     <ul class="lista-fotos">
+
       <li class="lista-fotos-item" v-for="foto in fotos">
 
-        <div class="painel">
-          <h2 class="painel-titulo">{{foto.titulo}}</h2>
-          <div class="painel-corpo">
-
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
-
-          </div><!-- fim painel-corpo -->
-        </div><!-- fim painel -->
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
 
       </li>
     </ul>
@@ -24,17 +18,26 @@
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue'
+
 export default {
-  name: 'app',
+
+  components: {
+
+    'meu-painel': Painel
+  },
+
   data () {
     return {
-      titulo: 'Bem vindes ao meu primeiro Vue.js App', 
+      titulo: 'App Pic', 
+
       fotos: []
     }
   },
-  created () {
+  created() {
 
-     this.$http.get('http://localhost:3001/v1/fotos')
+    this.$http
+      .get('http://localhost:3001/v1/fotos')
       .then(res => res.json())
       .then(fotos => this.fotos = fotos, err => console.log(err));
   }
@@ -63,28 +66,5 @@ export default {
 
   .imagem-responsiva {
     width: 100%;
-  }
-
-  /* estilo do painel */ 
-
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
   }
 </style>
