8207bfcca3c47b4469ca21e46bef534796570e8a
bianavic <bianavic@gmail.com>
2019-09-10 17:07:38 -0300

    estilos com escopos

--- app-pic/src/components/shared/painel/Painel.vue
+++ app-pic/src/components/shared/painel/Painel.vue
@@ -17,7 +17,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .painel {
     padding: 0 auto;
     border: solid 2px grey;
@@ -38,4 +38,9 @@ export default {
     padding: 10px;
     text-transform: uppercase;
   }
+
+* {
+    box-shadow: 5px 5px 5px;
+  }
+
 </style>
