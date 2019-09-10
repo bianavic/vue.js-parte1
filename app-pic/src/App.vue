a3efe17e20bb247b6ff28cd1fcbe701b91d01cf4
bianavic <bianavic@gmail.com>
2019-09-10 17:34:03 -0300

    computed property

--- app-pic/src/App.vue
+++ app-pic/src/App.vue
@@ -3,15 +3,14 @@
 
     <h1 class="centralizado">{{ titulo }}</h1>
 
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
+{{ filtro }}
     <ul class="lista-fotos">
-
-      <li class="lista-fotos-item" v-for="foto in fotos">
-
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
         </meu-painel>
-
-      </li>
+        </li>
     </ul>
 
   </div>
@@ -31,10 +30,27 @@ export default {
     return {
       titulo: 'App Pic', 
 
-      fotos: []
+      fotos: [],
+
+      filtro: ''
     }
   },
-  created() {
+
+  computed: {
+    fotosComFiltro() {
+
+      if (this.filtro) {
+        // criando uma expressão com o valor do filtro, insensitivo
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        // retorna apenas as fotos que condizem com a expressão
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  },
+
+  created () {
 
     this.$http
       .get('http://localhost:3001/v1/fotos')
@@ -65,6 +81,10 @@ export default {
   }
 
   .imagem-responsiva {
+    width: 100%;
+  }
+  .filtro {
+    display: block;
     width: 100%;
   }
 </style>
